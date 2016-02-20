bfbd75769711ea1432bc50e95bf4e31ab5be23ac
reinaldocomputer <felipeiex@hotmail.com>
2016-02-20 00:27:54 -0300

    Início de Projeto
    Classe Autômato.
    Classe AFD e AFND
    E main para entrada

---
+++ Autômatos de primeiro grau.cpp
@@ -0,0 +1,468 @@
+#include <math.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <bitset>
+#include <map>
+#define fore(i,n) for(int i = 0; i < n; i++)
+#define TAM 7
+
+
+/*
+Ex de entrada:
+
+2
+0 1
+4
+q1 q2 q3 q4
+3
+q1 u q2
+q2 u q3
+q3 0 q4
+1
+q1
+1
+q4
+AFND 1
+
+
+
+
+*/
+
+using namespace std;
+
+class Automato
+{
+
+protected:
+    vector< vector< bitset<TAM> > > tabelaDeTransicoes;
+
+public:
+    string nome;
+    map <string, int> Alfabeto,
+    Estados,
+    estadosIniciais,
+    estadosFinais;
+
+    Automato(string nome2, map <string, int> A, map <string, int> B, vector< vector< bitset<TAM> > > C, map <string, int> D, map <string, int> E)
+    {
+        nome = nome2;
+        Alfabeto = A;
+        Estados = B;
+        tabelaDeTransicoes = C;
+        estadosIniciais = D;
+        estadosFinais = E;
+    }
+
+    void getEstados()
+    {
+        cout << nome << endl << "Estados: " << endl;
+        map <string, int> :: iterator it = Estados.begin();
+
+        for(it; it != Estados.end(); it++)
+        {
+            cout << it->first << " " << it->second << endl;
+        }
+    }
+
+    void getEstadosIniciais()
+    {
+        cout << nome << endl << "Estados Iniciais: " << endl;
+        map <string, int> :: iterator it = estadosIniciais.begin();
+
+        for(it; it != estadosIniciais.end(); it++)
+        {
+            cout << it->first << " " << it->second << endl;
+        }
+
+    }
+
+    void getEstadosFinais()
+    {
+        cout << nome << endl << "Estados Finais: " << endl;
+        map <string, int> :: iterator it = estadosFinais.begin();
+
+        for(it; it != estadosFinais.end(); it++)
+        {
+            cout << it->first << " " << it->second << endl;
+        }
+
+    }
+
+
+    void getTabelaTransicao()
+    {
+        cout << nome << endl << "Tabela de Transicao: " << endl;
+        fore(i, tabelaDeTransicoes.size())
+        {
+            cout << i << " ";
+            fore(j, tabelaDeTransicoes[i].size())
+            {
+                cout << tabelaDeTransicoes[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    string imprimeBIT(bitset<TAM> &vetor)
+    {
+        string saida = "";
+        fore(i, vetor.size())
+        {
+            if(vetor[i])
+            {
+                string estado;
+                map <string, int> :: iterator it = Estados.begin();
+                for(it; it != Estados.end(); it++)
+                {
+                    if(it->second == i)
+                    {
+                        saida += it-> first;
+                        saida += " ";
+                    }
+                }
+            }
+        }
+        return saida;
+    }
+
+
+
+};
+
+class AFD: public Automato
+{
+public:
+    AFD(string nome, map <string, int> A, map <string, int> B, vector< vector< bitset<TAM> > > C, map <string, int> D, map <string, int> E): Automato(nome, A, B, C, D, E)
+    {
+    }
+};
+
+class AFND: public Automato
+{
+public:
+
+    AFND(string nome, map <string, int> A, map <string, int> B, vector< vector< bitset<TAM> > > C, map <string, int> D, map <string, int> E ): Automato(nome, A, B, C, D, E)
+    {
+    }
+
+
+    //Funcao que converte de não determinístico para determinístico
+    AFD AFND2FND()
+    {
+        map <string, int> a;
+        bitset <TAM> estados;
+
+
+        //Gerando o conjunto das partes
+        int N = pow(2,Estados.size());  //Quantidade de estados do FND
+
+        int contador = 0;
+
+        conjuntoDasPartes.insert(pair<string,int> (estados.to_string(), contador++));
+        fore(i, N - 1)
+        {
+            buildEstadosFND(estados, N, conjuntoDasPartes, contador);
+        }
+        map <string, int> :: iterator it = conjuntoDasPartes.begin();
+
+        string nomeFND = nome + " FND";
+        int qtdAlfabeto = Alfabeto.size();
+        int qtdEstados = conjuntoDasPartes.size();
+
+        vector< bitset<TAM> > estadosAFD;
+
+        it = conjuntoDasPartes.begin();
+
+        //Colocando em uma map por questão de padronização
+        for(it; it != conjuntoDasPartes.end(); it++)
+        {
+            bitset<TAM> aux (it->first);
+            estadosAFD.push_back(aux);
+        }
+
+
+        it = conjuntoDasPartes.begin();
+
+
+        //Gerando a Tabela de Transiação do FND
+        vector< vector< bitset<TAM> > > tabTransicaoFND(estadosAFD.size(), vector< bitset<TAM> >(qtdAlfabeto));
+        fore(i, estadosAFD.size())
+        {
+
+            vector<int> setados = checarSetados(estadosAFD[i], qtdEstados);
+            fore(j, qtdAlfabeto)
+            {
+                bitset<TAM> alf;
+                alf.set(); //seta todo mundo para 1.
+                fore(k, setados.size())
+                {
+                    alf = alf & tabelaDeTransicoes[setados[k]][j];
+                }
+                tabTransicaoFND[i][j] = alf;
+            }
+        }
+
+        map <string, int> estadosFinaisAFD;
+        it = estadosFinais.begin();
+
+        for(it; it != estadosFinais.end(); it++){
+                int posFinal = it->second;
+                map <string, int> :: iterator it2 = conjuntoDasPartes.begin();
+                for(it2; it2 != conjuntoDasPartes.end(); it2++){
+                    string aux = it2->first;
+                    bitset <TAM> aux2 (aux);
+                    if(aux2[posFinal]){
+                        pair <string, int> x (it2->first, it2->second);
+                        estadosFinaisAFD.insert(x);
+                    }
+
+                }
+
+        }
+
+        AFD AFD2 = AFD(nomeFND, Alfabeto, conjuntoDasPartes, tabTransicaoFND, estadosIniciais, estadosFinaisAFD);
+        return AFD2;
+    }
+
+private:
+
+    map <string, int> conjuntoDasPartes;
+
+
+    //Método para construir o conjunto das partes
+    void buildEstadosFND(bitset <TAM> &estados, int n, map <string, int> &vetor, int &contador)
+    {
+
+        int i = 0;
+        while( i < n )
+        {
+            if(estados[i])
+            {
+                estados.flip(i++);
+            }
+            else
+            {
+                estados.flip(i);
+                break;
+            }
+
+        }
+
+        if(i == n)
+        {
+            cout << "Overflow" << endl;
+        }
+        else
+        {
+            vetor.insert(pair <string, int>(estados.to_string<char,std::string::traits_type,std::string::allocator_type>(), contador++));
+        }
+    }
+
+    vector<int> checarSetados(bitset<TAM> set, int qtdEstados)
+    {
+
+        vector<int> resp;
+
+        fore(i, qtdEstados)
+        {
+            if(set[i]) resp.push_back(i);
+        }
+
+        return resp;
+
+    }
+
+
+};
+void trataVazio2(vector< vector< bitset<TAM> > > &transicoes, int i, bitset<TAM> cEstado, int vazio)
+{
+
+    int qtdEstados = transicoes.size();
+    int qtdSimbolos = transicoes[0].size();
+    vector<bool> mark(qtdEstados, false);
+    queue<int> fila;
+
+    for(int j=0; j<TAM; j++)
+    {
+        if(cEstado[j])
+        {
+            fila.push(j);
+            mark[j] = true;
+        }
+    }
+
+    while(!fila.empty())
+    {
+        int e = fila.front();
+        fila.pop();
+        bitset<TAM> aux = transicoes[e][vazio];
+        for(int j=0; j<TAM; j++)
+        {
+            if(aux[j])
+            {
+                if(!mark[j])
+                {
+                    mark[j] = true;
+                    fila.push(j);
+                }
+            }
+        }
+    }
+
+    for(int j=0; j<qtdEstados; j++)
+    {
+        if(mark[j])
+        {
+            for(int p=0; p<qtdSimbolos; p++)
+            {
+                transicoes[i][p] = transicoes[i][p] | transicoes[j][p];
+            }
+        }
+    }
+
+
+
+}
+
+void trataVazio(vector< vector< bitset<TAM> > > &transicoes, int vazio, int qtdEstados, int qtdAlfabeto)
+{
+
+     vector<bool> mark(qtdEstados, false);
+
+    /* Tratar partindo com vazio */
+    fore(i, transicoes.size())
+    {
+        if (transicoes[i][vazio].count())  /* Se o estado i receber algum símbolo vazio */
+        {
+            mark[i] = true;
+            bitset<TAM> cEstado = transicoes[i][vazio]; //Seleciona todos os estados em que o estado i se liga através do vazio.
+            trataVazio2(transicoes, i, cEstado, vazio);
+        }
+    }
+
+
+    /* Tratar chegando com vazio */
+    fore(i, transicoes.size()){
+      fore(j, qtdAlfabeto-1){
+         fore(k, mark.size()){
+            if(mark[k] && transicoes[i][j][k]){
+              transicoes[i][j] = transicoes[i][j] | transicoes[k][qtdAlfabeto-1];
+            }
+         }
+      }
+    }
+
+    /* Imprime o AFND depois de ajeitar os vazios */
+    cout<<"Tabela AFND: "<<endl;
+    for(int i=0; i<transicoes.size(); i++)
+    {
+        for(int j=0; j<transicoes[i].size()-1; j++)
+        {
+            cout<<transicoes[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+}
+
+
+int main()
+{
+    map <string, int> Alfabeto;
+    map <string,int> Estados;
+
+    //Quantidade de símbolos no alfabeto
+    int qtdAlfabeto, qtdEstados, qtdTransicoes;
+    cin >> qtdAlfabeto;
+
+    //Entrada de símbolos no alfabeto
+    fore(i, qtdAlfabeto)
+    {
+        string simbolo;
+        cin >> simbolo;
+        Alfabeto.insert(pair <string,int> (simbolo, i));
+    }
+    // Inserindo o -1 que irá representa o símbolo vazio
+    Alfabeto.insert(pair <string,int> ("-1", qtdAlfabeto));
+    qtdAlfabeto++;
+
+    //Quantidade de estados
+    cin >> qtdEstados;
+
+    //Leitura dos estados
+    fore(i,qtdEstados)
+    {
+        string estado;
+        cin >> estado;
+        Estados.insert(pair <string,int> (estado, i));
+    }
+
+    vector< vector< bitset<TAM> > > grafo(qtdEstados, vector< bitset<TAM> >(qtdAlfabeto));
+    //Quantidade de Transições
+    cin >> qtdTransicoes;
+    //Ler as transições
+    fore(i,qtdTransicoes)
+    {
+        string origem, simbolo, destino;
+        cin >> origem >> simbolo >> destino;
+        if(Alfabeto.find(simbolo) == Alfabeto.end())  /* Caso seja o símbolo vazio */
+        {
+            simbolo = "-1";
+        }
+        int origemid = (Estados.find(origem))->second;
+        int destinoid = (Estados.find(destino))->second;
+        int simboloid = (Alfabeto.find(simbolo))->second;
+        grafo[origemid][simboloid].set(destinoid); /* Se estiver no estado origemid e chegar o símboloid então vai para o estado destinoid. */
+    }
+    //Quantidade de estados iniciais
+    int qtdEstadosIniciais;
+    cin >> qtdEstadosIniciais;
+    map <string, int> estadosIniciais, estadosFinais;
+
+    //Leitura dos estados iniciais
+    fore(i, qtdEstadosIniciais)
+    {
+        string estadoInicial;
+        cin >> estadoInicial;
+        estadosIniciais.insert(pair<string,int> (estadoInicial, i));
+    }
+
+    //Quantidade de estados finais
+    int qtdEstadosFinais;
+    cin >> qtdEstadosFinais;
+
+    //Leitura dos estados finais
+    fore(i, qtdEstadosFinais)
+    {
+        string estadoFinal;
+        cin >> estadoFinal;
+        estadosFinais.insert(pair<string,int> (estadoFinal, i));
+
+    }
+    //Um nome para o autômato
+    string nome;
+    cin >> nome; /* Nome do Autômato Finito Não-Determinístico */
+
+ //Tratar o vazio
+    string simbolo = "-1";
+    int simboloid = (Alfabeto.find(simbolo))->second;
+    trataVazio(grafo, simboloid, qtdEstados, qtdAlfabeto);
+
+    AFND novo = AFND(nome, Alfabeto, Estados, grafo,estadosIniciais, estadosFinais);
+
+    //Transformando um AFND em AFD
+    AFD teste = novo.AFND2FND();
+    teste.getEstados();
+    cout << endl;
+    teste.estadosIniciais;
+    cout << endl;
+    teste.getTabelaTransicao();
+    cout << endl;
+    teste.getEstadosFinais();
+
+    /*   */
+}
